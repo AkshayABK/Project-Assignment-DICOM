7a7634cabc1ca561a8cb9cffa29873db720d9330
Akshay Kore <92500657+AkshayABK@users.noreply.github.com>
2025-01-28 04:07:24 +0530

    Add files via upload

--- utils/config.py
+++ utils/config.py
@@ -1,10 +1,4 @@
-dicom_attributes = {'attributes':["PatientName","PatientID","PatientBirthDate","PatientSex","StudyInstanceUID","StudyID","StudyDate","StudyTime","AccessionNumber","SeriesInstanceUID","SeriesNumber","Modality","SeriesDate","SeriesTime","SOPInstanceUID","InstanceNumber","ImagePositionPatient","ImageOrientationPatient","PixelSpacing","SliceThickness","SliceLocation","Manufacturer","ManufacturerModelName","DeviceSerialNumber"]
-                   }
-
-aws_credential = {
-    'aws_access_key_id':'your-accesss-key',
-    'aws_secret_access_key':'your-secrete-key'
-}
+dicom_attributes = {'attributes':["PatientID", "PatientName", "PatientBirthDate", "PatientSex", "PatientAge", "EthnicGroup","PatientWeight", "PatientSize", "StudyInstanceUID", "StudyDate", "StudyTime", "AccessionNumber", "ReferringPhysicianName", "StudyID", "StudyDescription", "PatientID", "SeriesInstanceUID", "SeriesNumber", "Modality", "SeriesDescription", "BodyPartExamined", "StudyInstanceUID", "SOPInstanceUID", "InstanceNumber", "ImageType", "AcquisitionDate", "AcquisitionTime", "PixelSpacing", "SliceThickness","SliceLocation","FrameOfReferenceUID", "SeriesInstanceUID", "Manufacturer", "ManufacturerModelName", "StationName", "DeviceSerialNumber", "SoftwareVersions", "ProtocolName", "ContrastBolusAgent", "ScanningSequence", "SequenceVariant", "ScanOptions", "Rows", "Columns", "BitsAllocated", "BitsStored", "HighBit", "PixelRepresentation", "PhotometricInterpretationInstitutionName", "InstitutionName", "InstitutionalDepartmentName", "FrameOfReferenceUID"]}
 
 
 aws_bucket_name = {
@@ -15,3 +9,81 @@ aws_bucket_name = {
 root_folder = 'lidc_small_dset/'
 
 transformed_file_location = '../transformed'
+
+datamart_attributes = {
+'patientInfo' : [
+    "PatientID",
+    "PatientName",
+    "PatientBirthDate",
+    "PatientSex",
+    "PatientAge",
+    "EthnicGroup",
+    "PatientWeight",
+    "PatientSize"
+],
+'studyInfo' : [
+    "StudyInstanceUID",
+    "PatientID",
+    "StudyDate",
+    "StudyTime",
+    "AccessionNumber",
+    "ReferringPhysicianName",
+    "StudyID",
+    "StudyDescription"
+],
+'seriesInfo' : [
+    "SeriesInstanceUID",
+    "StudyInstanceUID",
+    "SeriesNumber",
+    "Modality",
+    "SeriesDescription",
+    "BodyPartExamined"
+],
+'imageInfo' : [
+    "SOPInstanceUID",
+    "SeriesInstanceUID",
+    "InstanceNumber",
+    "ImageType",
+    "AcquisitionDate",
+    "AcquisitionTime",
+    "PixelSpacing",
+    "SliceThickness",
+    "SliceLocation",
+    "FrameOfReferenceUID"
+],
+'equipmentInfo' : [
+    "Manufacturer",
+    "ManufacturerModelName",
+    "StationName",
+    "DeviceSerialNumber",
+    "SoftwareVersions"
+],
+'procedureInfo' : [
+    "ProtocolName",
+    "ContrastBolusAgent",
+    "ScanningSequence",
+    "SequenceVariant",
+    "ScanOptions"
+],
+'pixelDataInfo' : [
+    "Rows",
+    "Columns",
+    "BitsAllocated",
+    "BitsStored",
+    "HighBit",
+    "PixelRepresentation",
+    "PhotometricInterpretationInstitutionName"
+],
+'miscInfo' : [
+    "InstitutionName",
+    "InstitutionalDepartmentName",
+    "FrameOfReferenceUID"
+]
+}
+
+datamart_primary_keys = {
+    'patientInfo' : "PatientID",
+    'studyInfo' : "StudyInstanceUID",
+    'seriesInfo' : "SeriesInstanceUID",
+    'imageInfo' : "SOPInstanceUID"
+}
