38604259888b1d5908773c9f5324ebae9b3068c6
Akshay Kore <akshaykore000@gmail.com>
2025-01-29 02:43:46 +0530

    Updated v2

--- script/main.py
+++ script/main.py
@@ -3,12 +3,11 @@ import os
 import pydicom
 import pandas as pd
 import boto3
-from io import BytesIO, StringIO
+from io import BytesIO
 import logging
-import io
 import time
 import threading
-import csv
+from pathlib import Path
 
 try:
     current_directory = os.getcwd()
@@ -19,23 +18,70 @@ except Exception as e:
     print(f"Error importing source folder path or utils path {e}")
 
 
-def logger(func):
-    def wrapper(*args, **kwargs):
-        print(f"Logger: {func.__name__} was called")
-        return func(*args, **kwargs)
-    return wrapper
+def setup_logging():
+    """
+    Sets up logging for the script with separate files for success and error logs.
+    """
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+
+    formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+
+    # Success log file
+    success_handler = logging.FileHandler("../logs/success.log")
+    success_handler.setLevel(logging.INFO)
+    success_handler.setFormatter(formatter)
+
+    # Error log file
+    error_handler = logging.FileHandler("../logs/error.log")
+    error_handler.setLevel(logging.ERROR)
+    error_handler.setFormatter(formatter)
+
+    # Adding handlers to the logger
+    logger.addHandler(success_handler)
+    logger.addHandler(error_handler)
+
+    return logger
+
+def generate_summary_from_folders(base_dir):
+    total_studies = 0
+    total_slices = 0
+    slices_per_study = []
+    slice_thickness_dist = {}
+
+    for patient_folder in Path(base_dir).iterdir():
+        if patient_folder.is_dir():  
+            patient_id = patient_folder.name  
+
+            for study_file in patient_folder.glob("*.csv"):
+                study_instance_uid = study_file.stem
+                
+                table = pd.read_csv(study_file)
+
+                if "SliceThickness" in table.columns:
+                    total_slices += table["SliceThickness"].sum()
+                    slices_per_study.append(table["SliceThickness"].count())
+                    slice_thickness_dist = table["SliceThickness"].describe().to_dict()
+
+                total_studies += 1
+    
+    data = {
+    "Total Studies": [total_studies],
+    "Total Slices Across All Scans": [total_slices],
+    "Average Number of Slices Per Study": [sum(slices_per_study) / len(slices_per_study) if slices_per_study else 0],
+    "Slice Thickness Distribution": [slice_thickness_dist]
+    }
+
+    df = pd.DataFrame(data)
+    
+    df.to_csv('Summary.csv', index=False)
+
+    return df
 
 
-@logger
 def extract_dicom_metadata(dicom_data):
     """
     Extracts specified DICOM metadata from a DICOM file and stores it in a dictionary.
-    
-    args: 
-        dicom_data (pydicom.dataset.FileDataset): DICOM data.
-        
-    returns:
-        dict: A dictionary containing the extracted DICOM metadata.
     """
     try:
         data = {}
@@ -43,78 +89,60 @@ def extract_dicom_metadata(dicom_data):
             data[attr] = getattr(dicom_data, attr, None)
         return data
     except Exception as e:
-        print(f"Error extracting DICOM metadata: {e}")
+        logger.error(f"Error extracting DICOM metadata: {e}")
         return None
 
 
-@logger
 def parse_dicom_file(obj):
     """
     Reads the DICOM file from the S3 object and converts it into a pandas DataFrame.
-    
-    args: 
-        obj (boto3.s3.Object): The S3 object containing the DICOM file.
-        
-    returns:
-        pd.DataFrame: DataFrame containing the DICOM metadata.
     """
     try:
         file_content = obj.get()['Body'].read()
         dicom_data = pydicom.dcmread(BytesIO(file_content))
-        print(f"Successfully read DICOM file: {obj.key}")
+        logger.info(f"Successfully read DICOM file: {obj.key}")
 
         metadata = extract_dicom_metadata(dicom_data)
         if metadata:
-            metadata["FilePath"] = obj.key
-            return pd.DataFrame([metadata])  # Directly returning a DataFrame
-        return None
+            return pd.DataFrame([metadata])  # Return DataFrame
 
     except Exception as e:
-        print(f"Failed to read file {obj.key} as DICOM: {e}")
-        return None
+        logger.error(f"Failed to read file {obj.key} as DICOM: {e}")
+        return pd.DataFrame()
 
 
 def save_df_to_local(df, local_dir, obj_key):
     """
     Saves the DICOM metadata DataFrame to a local CSV file. If the file already exists,
-    appends the new data.
-    
-    args: 
-        df (pd.DataFrame): The DataFrame containing the DICOM metadata.
-        local_dir (str): The local directory where the data should be saved.
-        obj_key (str): The key (name) of the S3 object.
-        
-    returns:
-        pd.DataFrame: The DataFrame that was saved (or appended).
+    appends the new data after removing duplicates.
     """
-    
     if df.shape[0] == 0:
-        print(f"Empty metadata list for {obj_key}")
-        return f"Empty metadata list for {obj_key}"
-    
+        logger.error(f"Empty metadata list for {obj_key}")
+        return
+
     try:
         PatientID = str(df['PatientID'][0])
         StudyInstanceUID = str(df['StudyInstanceUID'][0])
 
         patient_folder = os.path.join(local_dir, PatientID)
         os.makedirs(patient_folder, exist_ok=True)
-        
+
         local_file_path = os.path.join(patient_folder, f"{StudyInstanceUID}.csv")
 
         if os.path.exists(local_file_path):
             existing_df = pd.read_csv(local_file_path)
-            updated_df = pd.concat([existing_df, df], ignore_index=True)
+            combined_df = pd.concat([existing_df, df], ignore_index=True)
+            combined_df = combined_df.applymap(str)
+            combined_df.drop_duplicates(keep='last', inplace=True)
+            combined_df.to_csv(local_file_path, index=False)
+            logger.info(f"Appended data and removed duplicates in {local_file_path}")
         else:
-            updated_df = df
-
-        updated_df.to_csv(local_file_path, index=False)
-        print(f"Saved DataFrame to {local_file_path}")
-
-        return updated_df 
-    
+            # If the file does not exist, just save the new data
+            df.to_csv(local_file_path, index=False)
+            logger.info(f"Created and saved new file to {local_file_path}")
+        
     except Exception as e:
-        print(f"Failed to save DataFrame locally for {obj_key}: {e}")
-        return pd.DataFrame()
+        logger.error(f"Failed to save DataFrame locally for {obj_key}: {e}")
 
 
 def threaded_save_df_to_local(df, local_dir, obj_key):
@@ -123,59 +151,45 @@ def threaded_save_df_to_local(df, local_dir, obj_key):
     thread.start()
     return thread
 
+
 def save_to_datamart_layer(df, datamart_attributes):
     """
     Saves DataFrame to respective folders for analysis and transformation.
-    
-    args:
-        df (pd.DataFrame): The DataFrame containing the data.
-        datamart_attributes (dict): Dictionary containing the datamart categories as keys and the corresponding list of columns as values.
     """
     for key, value in datamart_attributes.items():
         output_folder = f'../Datamarts/{key}/'
-        os.makedirs(output_folder, exist_ok=True)  # Ensure the folder exists
+        os.makedirs(output_folder, exist_ok=True)
         output_csv_path = os.path.join(output_folder, f'{key}.csv')
 
         datamart_to_save = df[value]
 
-        if os.path.exists(output_csv_path):
-            existing_df = pd.read_csv(output_csv_path)
-            # This logic is for storing data only if it is new info
-            if key in config.datamart_primary_keys:
-                new_rows = datamart_to_save[~datamart_to_save[config.datamart_primary_keys[key]].isin([config.datamart_primary_keys[key]])]
+        try:
+            if os.path.exists(output_csv_path):
+                existing_df = pd.read_csv(output_csv_path)
+                combined_df = pd.concat([existing_df, datamart_to_save], ignore_index=True).drop_duplicates()
+                combined_df.to_csv(output_csv_path, index=False)
+                logger.info(f"Appended data to {output_csv_path}")
             else:
-                new_rows = datamart_to_save
-                
-            if not new_rows.empty:
-                new_rows.to_csv(output_csv_path, mode='a', header=False, index=False)
-                print(f"Appended new data to {output_csv_path}")
-            else:
-                print(f"No new {config.datamart_primary_keys[key]} data to append for {key}")
-                
-            # Dropping duplicates   
-            existing_df = pd.read_csv(output_csv_path)
-            existing_df = existing_df.drop_duplicates()
-            
-        else:
-            datamart_to_save.to_csv(output_csv_path, index=False)
-            print(f"Data has been saved to {output_csv_path}")
+                datamart_to_save.to_csv(output_csv_path, index=False)
+                logger.info(f"Created and saved data to {output_csv_path}")
+        except Exception as e:
+            logger.error(f"Failed to save to datamart layer {key}: {e}")
 
-def threaded_save_to_datamart_layer(transformed_df, datamart_attributes):
-    """Wrapper function to run threaded_save_to_datamart_layer in a new thread."""
-    thread = threading.Thread(target=save_to_datamart_layer, args=(transformed_df, datamart_attributes))
+
+def threaded_save_to_datamart_layer(df, datamart_attributes):
+    """Wrapper function to run save_to_datamart_layer in a new thread."""
+    thread = threading.Thread(target=save_to_datamart_layer, args=(df, datamart_attributes))
     thread.start()
     return thread
-            
+
 
 def main():
-
     os.environ['ACCESS_KEY'] = 'your-access-key'
     os.environ['SECRET_KEY'] = 'your-secret-key'
 
     aws_access_key_id = os.getenv('ACCESS_KEY')
     aws_secret_access_key = os.getenv('SECRET_KEY')
 
-    print(aws_access_key_id, aws_secret_access_key)
     s3 = boto3.resource(
         service_name='s3',
         region_name='eu-north-1',
@@ -185,27 +199,28 @@ def main():
 
     df = pd.DataFrame()
     raw_bucket_name = config.aws_bucket_name.get('raw', '')
-    raw_transformed_name = config.aws_bucket_name.get('transformed', '')
     root_folder_name = config.root_folder
     transformed_folder_name = config.transformed_file_location
     raw_bucket = s3.Bucket(raw_bucket_name)
+
     start_time = time.time()
-    
+
     threads = []
-    
+
     for obj in raw_bucket.objects.filter(Prefix=root_folder_name):
         if not obj.key.endswith('/'):
-            print(f'Processing {obj.key} file')
+            print(f'Processing file: {obj.key}')
+            logger.info(f'Processing file: {obj.key}')
             metadata = parse_dicom_file(obj)
             if metadata is not None:
-                # Run save_df_to_local in a separate thread
                 thread = threaded_save_df_to_local(metadata, transformed_folder_name, obj.key)
                 threads.append(thread)
-                print('**********************************************')
 
     for thread in threads:
         thread.join()
-        
+
+    logger.info(f"Data successfully processed and saved to the transformed folder: {transformed_folder_name}")
+    
     datamart_threads = []
     
     for root, dirs, files in os.walk(transformed_folder_name):
@@ -215,15 +230,23 @@ def main():
                 print(f"Processing {csv_file_path}")
                 transformed_df = pd.read_csv(csv_file_path)
                 datamart_thread = threaded_save_to_datamart_layer(transformed_df, config.datamart_attributes)
-                datamart_threads.append(thread)
+                datamart_threads.append(datamart_thread)
                 
     for datamart_thread in datamart_threads:
         datamart_thread.join()
+     
+    #Generating summary
+    generate_summary_from_folders('../summary')
 
     end_time = time.time()
-    execution_time = end_time - start_time
-    print(f'Total execution time {execution_time}')
+    logger.info(f'Total execution time: {end_time - start_time} seconds')
 
 
-if __name__ == '__main__':    
-    main()
+if __name__ == '__main__':
+    logger = setup_logging()
+    logger.info("Starting the script...")
+    try:
+        main()
+        logger.info("Script completed successfully.")
+    except Exception as e:
+        logger.error(f"Script failed: {e}")
